1ed532d7552af02ede0156023ecb8e009a00bc73
ywen <yaliyingwy@gmail.com>
2015-05-03 16:28:39 +0800

    add json support

---
+++ YwenKit/categories/common/NSDictionary+WYExt.h
@@ -0,0 +1,15 @@
+//
+//  NSDictionary+WYExt.h
+//  YwenKit
+//
+//  Created by ywen on 15/5/3.
+//  Copyright (c) 2015年 ywen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSDictionary (WYExt)
+
++(NSString *) WY_ToJson;
+
+@end
